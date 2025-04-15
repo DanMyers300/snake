841045b8bf83ce0b047a52e164e05cd9c7ead782
dan <contact@danmyers.net>
2025-04-15 14:42:35 -0500

    Added basic flake

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,38 @@
 use std::f64;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(start)]
-fn start() {
-  let document = web_sys::window().unwrap().document().unwrap();
-  let canvas = document.get_element_by_id("canvas").unwrap();
-  let canvas: web_sys::HtmlCanvasElement = canvas
-    .dyn_into::<web_sys::HtmlCanvasElement>()
-    .map_err(|_| ())
-    .unwrap();
-
-  let context = canvas
-    .get_context("2d")
-    .unwrap()
-    .unwrap()
-    .dyn_into::<web_sys::CanvasRenderingContext2d>()
-    .unwrap();
-
-  context.begin_path();
-
-  // Draw the border
-  context.set_line_width(4.0);
-  context.stroke_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
-
-  context.stroke();
+pub struct Game {
+  snake: Vec<(u32, u32)>,
+  food: (u32, u32),
+  direction: (i32, i32),
+  width: u32,
+  height: u32
 }
+
+#[wasm_bindgen]
+impl Game {
+  pub fn draw() {
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+      .dyn_into::<web_sys::HtmlCanvasElement>()
+      .map_err(|_| ())
+      .unwrap();
+  
+    let context = canvas
+      .get_context("2d")
+      .unwrap()
+      .unwrap()
+      .dyn_into::<web_sys::CanvasRenderingContext2d>()
+      .unwrap();
+  
+    context.begin_path();
+  
+    // Draw the border
+    context.set_line_width(4.0);
+    context.stroke_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
+  
+    context.stroke();
+  }
+}
+
